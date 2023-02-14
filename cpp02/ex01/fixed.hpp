6c65062a548f6562795ab8b1cad106055fef9f43
Mohammed Habibi ihlane <mhabibi-@e2r4p10.1337.ma>
2023-02-14 23:44:57 +0100

    workin on cpp02

--- cpp02/ex01/fixed.hpp
+++ cpp02/ex01/fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhabibi- <mhabibi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/10 21:44:28 by mhabibi-          #+#    #+#             */
-/*   Updated: 2023/02/14 16:10:24 by mhabibi-         ###   ########.fr       */
+/*   Created: 2023/02/14 14:26:34 by mhabibi-          #+#    #+#             */
+/*   Updated: 2023/02/14 15:59:30 by mhabibi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,14 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
-class fixed
+class Fixed
 {
-private:
-    int value;
-    static const int fract_bit = 8;
-public:
-    fixed()
-    {
-        std::cout << "Default constructor Called\n";
-        this->value = 0;
-    }
-    fixed(fixed const &src)
-    {
-        std::cout << "copy constructor Called\n";
-        this->value = src.value;
-    }
-    ~fixed();
-    int getRawBits(void) const
-    {
-        return(value);
-    }
-    void    setRawBits(int const raw)
-    {
-        this->value = raw;
-    }
+    
 };
+
+
+
 
 #endif
