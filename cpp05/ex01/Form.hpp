2f9dd78a00fd1073c9bb81cb4345d6710a48ceaf
mihlane <samohib50@gmail.com>
2023-03-03 20:56:10 +0100

    005

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhabibi- <mhabibi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/02 16:12:10 by mhabibi-          #+#    #+#             */
-/*   Updated: 2023/03/03 15:05:04 by mhabibi-         ###   ########.fr       */
+/*   Created: 2023/03/03 15:38:09 by mhabibi-          #+#    #+#             */
+/*   Updated: 2023/03/03 16:21:26 by mhabibi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef Form_HPP
+# define Form_HPP
+
+# include <iostream>
+
+class Form
+{
+    private:
+    std::string const name;
+    bool isSigned;
+    const int gradeToSign;
+    const int gradeToExecute;
+    public:
+    Form();
+    ~Form();
+    Form(std::string name, int gradeToSign, int gradeToExecute);
+};
+#endif
