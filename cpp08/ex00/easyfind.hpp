a7d9fb3d155ca0c3878ceee0e80f1c2869874386
mihlane <samohib50@gmail.com>
2023-04-14 19:27:33 +0000

    cpp08 ex00 need just main

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhabibi- <mhabibi-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 18:02:08 by mhabibi-          #+#    #+#             */
-/*   Updated: 2023/04/11 13:45:13 by mhabibi-         ###   ########.fr       */
+/*   Updated: 2023/04/14 17:26:21 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,14 @@
 
 template <typename Container>
 
-void easyfind(Container &x, int i)
+void easyfind(Container &container, int n)
 {
-    // std:: it
-    int *k;
-    typename Container::iterator begin = x.begin();
-    typename Container::iterator end = x.end();
-     typename Container::iterator = std::find(begin, end, i);
-    std::cout << k << i << end << begin << std::endl;
-    // T s;
-    // s = x;
-    // x = y;
-    // y = s;
+    typename Container::iterator it;
+    it = std::find(container.begin(), container.end(), n);
+    if (it != container.end())
+        std::cout << "Found " << n << " in container" << std::endl;
+    else
+        std::cout << "Not found " << n << " in container" << std::endl;
 }
 
 #endif
