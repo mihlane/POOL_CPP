294aad38f5e211fdef15c9d69ad224ffb2e391a1
mihlane <samohib50@gmail.com>
2023-05-14 22:21:05 +0100

    0999

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mhabibi- <mhabibi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:21:38 by mhabibi-          #+#    #+#             */
-/*   Updated: 2023/05/10 15:37:52 by mhabibi-         ###   ########.fr       */
+/*   Updated: 2023/05/14 17:57:27 by mhabibi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,17 @@ int main(int ac, char **av)
 
         if (ss >> date_str >> sep >> value && sep == '|' && !ss.fail()) {
             data[date_str] = value;
-            std::cout << line << std::endl;
+            std::string substr = "-";
+        for(int i = 0; i <int(date_str.length()); i++)
+        {
+    size_t pos = date_str.find(substr); // Find the position of the substring
+    if (pos != std::string::npos) { // Check if the substring exists in the string
+        date_str.replace(pos, substr.length(), "");
+    }
+    else 
+            break; // Replace the substring with an empty string
+        }
+            std::cout << date_str << std::endl;
         } else {
             std::cerr << "Error: invalid input line: " << line << std::endl;
         }
