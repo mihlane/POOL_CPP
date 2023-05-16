5dee95a7fcb5c6becc96c84889db3e608e14075b
mihlane <samohib50@gmail.com>
2023-05-16 23:40:56 +0100

    090

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mhabibi- <mhabibi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:21:38 by mhabibi-          #+#    #+#             */
-/*   Updated: 2023/05/14 17:57:27 by mhabibi-         ###   ########.fr       */
+/*   Updated: 2023/05/15 15:06:36 by mhabibi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,26 +31,25 @@ int main(int ac, char **av)
         while (std::getline(inputFile, line)) 
         {
             std::stringstream ss(line);
-        std::string date_str;
-        double value;
-        char sep;
-
-        if (ss >> date_str >> sep >> value && sep == '|' && !ss.fail()) {
-            data[date_str] = value;
-            std::string substr = "-";
-        for(int i = 0; i <int(date_str.length()); i++)
-        {
-    size_t pos = date_str.find(substr); // Find the position of the substring
-    if (pos != std::string::npos) { // Check if the substring exists in the string
-        date_str.replace(pos, substr.length(), "");
-    }
-    else 
-            break; // Replace the substring with an empty string
-        }
-            std::cout << date_str << std::endl;
-        } else {
+            std::string date_str;
+            double value;
+            char sep;
+                std::string substr = "-";
+            if (ss >> date_str >> sep >> value && sep == ',' && !ss.fail()) 
+            {
+                data[date_str] = value;
+                for(int i = 0; i <int(date_str.length()); i++)
+                {
+                    size_t pos = date_str.find(substr); // Find the position of the substring
+                    if (pos != std::string::npos) // Check if the substring exists in the string
+                        date_str.replace(pos, substr.length(), "");
+                    else 
+                        break; // Replace the substring with an empty string
+                }
+                std::cout << date_str << std::endl;
+            } 
+            else
             std::cerr << "Error: invalid input line: " << line << std::endl;
-        }
         }
             inputFile.close();
     }
